ae00dc4caf993ed367a26024bbe9ffc807ba1468
Felipe Prestes <feprestes00@gmail.com>
2024-04-24 14:50:34 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Felipe Prestes on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
